156a82847ebdf0728c362727c921902ac901e6fa
dhoRam <dhondiram.jadhav@lntinfotech.com>
2020-08-14 20:37:20 +0530

    query changes

--- src/main/java/com/lti/mod/services/authservice/repository/UserRepository.java
+++ src/main/java/com/lti/mod/services/authservice/repository/UserRepository.java
@@ -2,14 +2,19 @@ package com.lti.mod.services.authservice.repository;
 
 import com.lti.mod.services.authservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByName(String name);
 
-    Optional<User> findByEmail(String email);
+  //  Optional<User> findByEmail(String email);
+    @Query(value = "SELECT * from User e where e.email =:email and e.status='0'", nativeQuery = true)
+    Optional<User> findByEmail(@Param("email") String email);
 
 
 
